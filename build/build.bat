79fd8ecf4d07493bfa20fa784364954e1d7ff006
Stefan <cezarst42@gmail.com>
2022-07-10 07:21:26 +0200

    Started abstracting old code

--- build/build.bat
+++ build/build.bat
@@ -1,3 +1,3 @@
 @echo off
-g++ -o main.exe ..\src\main.cpp ..\src\shader.cpp ..\src\buffer.cpp ..\src\textureLoader.cpp ..\src\stb_image.cpp ..\src\glad.c -llibassimp -lopengl32 -lglfw3 -lgdi32
+g++ -o main.exe ..\src\main.cpp ..\src\stb_image.cpp ..\src\glad.c -llibassimp -lopengl32 -lglfw3 -lgdi32
 
