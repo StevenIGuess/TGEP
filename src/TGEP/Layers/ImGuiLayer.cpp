b405ff7a54f2d72b8263968e30c09779a4035379
StevenIGuess <stefan.cubing242@gmail.com>
2022-07-12 23:50:59 +0200

    started fixing event

--- src/TGEP/Layers/ImGuiLayer.cpp
+++ src/TGEP/Layers/ImGuiLayer.cpp
@@ -9,6 +9,7 @@
 namespace TGEP
 {
 
+
     ImGuiLayer::ImGuiLayer() : Layer("ImGuiLayer")
     {
 
@@ -27,6 +28,31 @@ namespace TGEP
         ImGuiIO &io = ImGui::GetIO();
         io.BackendFlags = ImGuiBackendFlags_HasMouseCursors;
         io.BackendFlags = ImGuiBackendFlags_HasSetMousePos;
+
+        io.KeyMap[ImGuiKey_Tab] = GLFW_KEY_TAB;
+        io.KeyMap[ImGuiKey_LeftArrow] = GLFW_KEY_LEFT;
+        io.KeyMap[ImGuiKey_RightArrow] = GLFW_KEY_RIGHT;
+        io.KeyMap[ImGuiKey_UpArrow] = GLFW_KEY_UP;
+        io.KeyMap[ImGuiKey_DownArrow] = GLFW_KEY_DOWN;
+        io.KeyMap[ImGuiKey_PageUp] = GLFW_KEY_PAGE_UP;
+        io.KeyMap[ImGuiKey_PageDown] = GLFW_KEY_PAGE_DOWN;
+        io.KeyMap[ImGuiKey_Home] = GLFW_KEY_HOME;
+        io.KeyMap[ImGuiKey_End] = GLFW_KEY_END;
+        io.KeyMap[ImGuiKey_Insert] = GLFW_KEY_INSERT;
+        io.KeyMap[ImGuiKey_Delete] = GLFW_KEY_DELETE;
+        io.KeyMap[ImGuiKey_Backspace] = GLFW_KEY_BACKSPACE;
+        io.KeyMap[ImGuiKey_Space] = GLFW_KEY_SPACE;
+        io.KeyMap[ImGuiKey_Enter] = GLFW_KEY_ENTER;
+        io.KeyMap[ImGuiKey_Escape] = GLFW_KEY_ESCAPE;
+        io.KeyMap[ImGuiKey_KeyPadEnter] = GLFW_KEY_KP_ENTER;
+        io.KeyMap[ImGuiKey_A] = GLFW_KEY_A;
+        io.KeyMap[ImGuiKey_C] = GLFW_KEY_C;
+        io.KeyMap[ImGuiKey_V] = GLFW_KEY_V;
+        io.KeyMap[ImGuiKey_X] = GLFW_KEY_X;
+        io.KeyMap[ImGuiKey_Y] = GLFW_KEY_Y;
+        io.KeyMap[ImGuiKey_Z] = GLFW_KEY_Z;
+
+
 
         ImGui_ImplOpenGL3_Init("#version 460");
 
@@ -108,10 +134,9 @@ namespace TGEP
     bool ImGuiLayer::OnKeyPressedEvent(KeyPressedEvent &e)
     {
         ImGuiIO& io = ImGui::GetIO();
-        ImGuiKey imgui_key = TGEPKeycodeToImGuiKeycode(e.GetKeyCode());
-        io.AddKeyEvent(imgui_key, GLFW_PRESS);
+        io.KeysDown[e.GetKeyCode()] = true;
 
-        LOG_CORE_INFO("KEY PRESS {0}", TGEPKeycodeToImGuiKeycode(e.GetKeyCode()));
+        LOG_CORE_INFO("KEY PRESS {0}", e.GetKeyCode());
 
         return false; //not sure if already handled
     }
@@ -119,10 +144,9 @@ namespace TGEP
     bool ImGuiLayer::OnKeyReleasedEvent(KeyReleasedEvent &e)
     {
         ImGuiIO& io = ImGui::GetIO();
-        ImGuiKey imgui_key = TGEPKeycodeToImGuiKeycode(e.GetKeyCode());
-        io.AddKeyEvent(imgui_key, GLFW_RELEASE);
+        io.KeysDown[e.GetKeyCode()] = false;
 
-        LOG_CORE_INFO("KEY RELEASE {0}", TGEPKeycodeToImGuiKeycode(e.GetKeyCode()));
+        LOG_CORE_INFO("KEY RELEASE {0}", e.GetKeyCode());
 
         return false; //not sure if already handled
     }
