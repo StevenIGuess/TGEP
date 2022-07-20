0bdc380fe27eda496eaa5d2a43abd50a227ca683
StevenIGuess <stefan.cubing242@gmail.com>
2022-07-20 15:56:42 +0200

    fixed build file

--- build/build.bat
+++ build/build.bat
@@ -5,13 +5,15 @@ if not exist "imgui.o" (
 
     echo COMPILING IMGUI
 
-    g++ -c ..\src\TGEP\ImGui\imgui.cpp
-    g++ -c ..\src\TGEP\ImGui\imgui_draw.cpp
-    g++ -c ..\src\TGEP\ImGui\imgui_tables.cpp
-    g++ -c ..\src\TGEP\ImGui\imgui_widgets.cpp
-    g++ -c ..\src\TGEP\ImGui\imgui_impl_opengl3.cpp
-    g++ -c ..\src\TGEP\ImGui\imgui_demo.cpp
-    g++ -c ..\src\TGEP\ImGui\imgui_impl_glfw.cpp
+    g++ -c ..\vendor\imgui\
+
+    g++ -c ..\vendor\imgui\imgui.cpp
+    g++ -c ..\vendor\imgui\imgui_draw.cpp
+    g++ -c ..\vendor\imgui\imgui_tables.cpp
+    g++ -c ..\vendor\imgui\imgui_widgets.cpp
+    g++ -c ..\vendor\imgui\backends\imgui_impl_opengl3.cpp
+    g++ -c ..\vendor\imgui\imgui_demo.cpp
+    g++ -c ..\vendor\imgui\backends\imgui_impl_glfw.cpp
 )
 
 if not exist "glad.o" (
