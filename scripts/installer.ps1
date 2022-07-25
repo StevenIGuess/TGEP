d07b108cc6efdef9451873349e1401a0360460b7
StevenIGuess <stefan.cubing242@gmail.com>
2022-07-25 17:06:18 +0200

    added path vars

--- scripts/installer.ps1
+++ scripts/installer.ps1
@@ -2,6 +2,8 @@ Install-Module -Name 7Zip4PowerShell -Force
 
 (New-Object Net.WebClient).DownloadFile('https://github.com/niXman/mingw-builds-binaries/releases/download/12.1.0-rt_v10-rev3/x86_64-12.1.0-release-win32-seh-rt_v10-rev3.7z', '.\mingw64.7z');
 
-Expand-7Zip .\mingw64.7z -TargetPath .\
+Expand-7Zip .\mingw64.7z -TargetPath C:\mingw64\
 
 del mingw64.7z
+
+[Environment]::SetEnvironmentVariable('PATH', ${env:path} + ';C:\mingw64\', 'Machine')
